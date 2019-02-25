0a0b95a26a2d78ed1700648467847be25def7d0b
Vikas babu. pawale <vikas.babu@accolite.com>
2019-02-25 17:29:03 +0530

    containers

--- shark/src/app/app-routing.module.ts
+++ shark/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './components/signup/signup.component';
+import { PlaygroundComponent } from './components/playground/playground.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+   path: 'playground',
+   component: PlaygroundComponent 
+  },
+  {
+    path: 'instruction',
+    component: PlaygroundComponent 
+   }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- shark/src/app/app.module.ts
+++ shark/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { PlaygroundComponent } from './components/playground/playground.component';
+import { InstructionComponent } from './components/instruction/instruction.component';
+import { MainHeaderComponent } from './components/main-header/main-header.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignupComponent,
+    PlaygroundComponent,
+    InstructionComponent,
+    MainHeaderComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
